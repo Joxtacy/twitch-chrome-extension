a605e532f2300f1f6347f8d7dfe2ce51e537a5f7
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-07-13 13:23:11 +0900

    Update clicker

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 setInterval(() => {
-    els = document.getElementsByClassName("tw-button tw-button--success tw-interactive");
+    els = document.querySelectorAll('[aria-label="Claim Bonus"]');
     el = els && els[0];
     if (el != null) {
         today = new Date();
@@ -8,7 +8,47 @@ setInterval(() => {
         ss = s < 10 ? "0" + s : s;
         mm = m < 10 ? "0" + m : m;
         time = today.getHours() + ":" + mm + ":" + ss
-        console.info("It got clicked!", time);
+        console.info("[TWITCH CLICKER]: It got clicked!", time);
         el.click();
+    } else {
+        console.warn("[TWITCH CLICKER]: Tried to click the button, but couldn't find one");
     }
-}, 60000);
+}, 10000);
+
+/*
+nodeList = document.getElementsByClassName("community-points-summary tw-align-items-center tw-flex tw-full-height");
+targetNode = nodeList && nodeList.length && nodeList[0];
+config = { attributes: false, childList: true, subtree: true };
+
+observerCallback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            els = document.getElementsByClassName("tw-button tw-button--success tw-interactive");
+            el = els && els[0];
+            if (el != null) {
+                today = new Date();
+                s = today.getSeconds();
+                m = today.getMinutes();
+                ss = s < 10 ? "0" + s : s;
+                mm = m < 10 ? "0" + m : m;
+                time = today.getHours() + ":" + mm + ":" + ss
+                el.click();
+            }
+        }
+    }
+};
+
+if (targetNode) {
+    observer = new MutationObserver(observerCallback);
+    observer.observe(targetNode, config);
+} else {
+    setTimeout(function() {
+        nodeList = document.getElementsByClassName("community-points-summary tw-align-items-center tw-flex tw-full-height");
+        targetNode = nodeList && nodeList.length && nodeList[0];
+        observer = new MutationObserver(observerCallback);
+        observer.observe(targetNode, config);
+    }, 15000);
+}
+*/
+
